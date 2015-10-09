de61600627ade88ec423ab91bbec3acf7793db5b
Son Pham <sonpa1993@gmail.com>
2015-10-09 14:14:11 -0500

    TODO

--- assets/js/app.js
+++ assets/js/app.js
@@ -106,7 +106,7 @@ app.controller('404Ctrl', ['$scope', '$rootScope', function($scope, $rootScope) 
 app.controller('ClassCtrl', ['$scope', '$rootScope', '$routeParams', function($scope, $rootScope, $routeParams) {
   var class_name = $routeParams.class_name
 
-  // Make an API to backend here
+  // TODO: Make an API call to backend here
 
   $rootScope.root = {
     title: 'Class ' + class_name + ' | GitSubmit'
